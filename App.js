413f4dacb7d6ac0ba630ccd18239cb47d248b650
Matias Delorenzi <matiasdelorenzi1997@gmail.com>
2022-02-19 22:39:01 -0300

    Created basic app

---
+++ App.js
@@ -0,0 +1,90 @@
+import React, { useEffect, useState } from 'react'
+import CoinItem from './components/CoinItem'
+import { 
+  View,
+  Text,
+  FlatList,
+  StyleSheet,
+  TextInput,
+  StatusBar
+} from 'react-native'
+
+const App = () => {
+
+    const [coins, setCoins] = useState([])
+    const [search, setSearch] = useState([])
+    const [refreshing, setRefreshing] = useState(false)
+      
+    const loadCoins = async () => {
+      const res = await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+      const data = await res.json();
+      setCoins(data);
+    };
+
+    useEffect( () => {
+      loadCoins();
+    }, []);
+
+    return (
+      <View style={styles.container}>
+        <StatusBar backgroundColor="#141414"/>
+        <View style={styles.header} >
+          <Text style={styles.title}>CryptoMarket</Text>
+          <TextInput style={styles.searchInput}
+            placeholder='Search a coin'
+            placeholderTextColor="#858585"
+            onChangeText={text => setSearch(text.toLowerCase())}
+          />
+        </View>
+        <FlatList
+          style={styles.list}
+          data={coins.filter(
+            (coin) => 
+              coin.name.toLowerCase().includes(search) || 
+              coin.symbol.toLowerCase().includes(search)
+          )}
+          renderItem={({ item }) => {
+            return <CoinItem coin={item}/>
+          }}
+          showsVerticalScrollIndicator={false}
+          refreshing={refreshing}
+          onRefresh={async () => {
+            setRefreshing(true)
+            await loadCoins()
+            setRefreshing(false)
+          }}
+        />
+      </View>
+    )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#141414',
+    alignItems: 'center',
+    flex: 1
+  },
+  title: {
+    color: '#ffffff',
+    marginTop: 30,
+    fontSize: 20,
+  },
+  list: {
+    width: '90%'
+  },
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '90%',
+    marginBottom: 15
+  },
+  searchInput:{
+    color: '#ffffff',
+    borderBottomColor: '#4657ce',
+    borderBottomWidth: 1,
+    width: '40%',
+    textAlign: 'center'
+  }
+})
+
+export default App;
